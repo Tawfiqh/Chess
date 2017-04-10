426a415dc09ba69980616abd145f7b4989c6bfa6
Tawfiq H <Tawfiq.Hamid@gmail.com>
2017-04-10 19:26:18 +0100

    drawing a chess board

--- chess.js
+++ chess.js
@@ -2,14 +2,33 @@
 console.log("starting up");
 $( "#ChessBoard" ).css( "border", "3px solid white" );
 
-var num = "4";
-   var board = $( "#ChessBoard" );
 
-   for(var i = 1; i <= num; i++) {
-       container.append('<div id="mystyle" class="pic'+i+'" />');
-   }
-   
-   $('#divsholder').html(container);
+
+var rows = "8";
+var columns = "8";
+var board = $( "#ChessBoard" );
+
+var i =0;
+for(var r = 0; r < rows; r++) {
+  for(var c = 0; c < columns; c++) {
+
+    if(c%2 == 0 )
+      col = -1
+    else
+      col = 1
+
+    if(r%2 == 0)
+        col = col *-1
+
+  col  = (col == 1) ? 'white' : 'black';
+
+
+  board.append('<div id="grid' + i + '" class="grid ' +col +'" />');
+  i++;
+}
+}
+
+//   $('#divsholder').html(container);
 
 
 })();
