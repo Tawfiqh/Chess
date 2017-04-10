501bf2be3f42e4bb36983a3c114033c47da00bdc
Tawfiq H <Tawfiq.Hamid@gmail.com>
2017-04-10 19:10:03 +0100

    Skeleton setup

---
+++ chess.js
@@ -0,0 +1,15 @@
+;(function() {
+console.log("starting up");
+$( "#ChessBoard" ).css( "border", "3px solid white" );
+
+var num = "4";
+   var board = $( "#ChessBoard" );
+
+   for(var i = 1; i <= num; i++) {
+       container.append('<div id="mystyle" class="pic'+i+'" />');
+   }
+   
+   $('#divsholder').html(container);
+
+
+})();
